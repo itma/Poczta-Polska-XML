3b06cbdb12fb187d5a3618089467701554775379
Andrew <andrew@andrew-developer.(none)>
2012-09-20 11:42:16 +0200

    class skeleton

---
+++ PocztaPolskaXML.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Klasa PocztaPolskaXML sluzy do generowania pliku XML zgodnego ze standardem EKN. Plik
+ * XML zwyczajowo wykorzystywany do komunikacji Klient - Poczta Polska
+ * 
+ * @author Andrzej Bernat <andrzej@itma.pl>
+ */
+class PocztaPolskaXML {
+    
+}
+
+?>
